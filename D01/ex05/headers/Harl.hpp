0efa320b09f62d426dca6267664fb2a66ed1345d
Waydenn <rodrigueztheo921@gmail.com>
2024-01-23 18:41:42 +0100

    hash function is now private

--- D01/ex05/headers/Harl.hpp
+++ D01/ex05/headers/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: wayden <wayden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 16:59:03 by wayden            #+#    #+#             */
-/*   Updated: 2024/01/23 18:06:08 by wayden           ###   ########.fr       */
+/*   Updated: 2024/01/23 18:40:36 by wayden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ class Harl{
 public:
 	Harl();
 	~Harl();
-	int	hash(const std::string& str);
 	void complain(std::string level);	
 private:
+	int	hash(const std::string& str);
 	void debug( void );
 	void info( void );
 	void warning( void );
